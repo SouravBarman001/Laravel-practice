626f489c0e8c92e0b5aea963bbdb52d3a9d8dea4
Sourav <sourav.iit.nstu@gmail.com>
2022-07-09 10:43:31 +0600

    view logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     function loadView($user){
-        return view('users',['name'=>$user]);
-    }
+
+       
+            try {
+        
+                return view('users1',['name'=>$user]);  //view name is testing but i have given testing1
+        
+              } catch (\Exception $e) {  //here we catch that exception file not found and handle it
+        
+                  return "Page not found";
+        
+          
+
+      //  return view('users',['name'=>$user]);
+    }}
 }
